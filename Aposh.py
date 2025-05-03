3f83607d30455279a1aa83a3a0bc0386dc4a6bc9
Andrii Oshi <oshiworkmail@gmail.com>
2025-05-03 21:55:43 +0300

    Forgor about main file lmfao

--- Aposh.py
+++ Aposh.py
@@ -8,7 +8,11 @@ import random
 import time
 import humanfriendly
 from datetime import datetime, timezone, timedelta
+from cap_game import setup
 
+
+intents = discord.Intents.default()
+intents.members = True
 
 # --- Конфигурация бота ---
 bot = commands.Bot(
@@ -27,12 +31,14 @@ import answers
 import actions
 import modcom
 import helpcom
+import cap_game
 
 # Передаем бота в модули
 answers.setup(bot)
 actions.setup(bot)
 modcom.setup(bot)
 helpcom.setup(bot)
+cap_game.setup(bot)
 
 load_dotenv()
 test_mode = False  # Режим тестирования (без реальных действий)
