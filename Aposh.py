7c32dfce363ff2f28f2b3a1c5c91a8407754d704
Andrii Oshi <oshiworkmail@gmail.com>
2025-05-27 13:45:34 +0300

    На всякий слчай коммит всего + started utils module

--- Aposh.py
+++ Aposh.py
@@ -8,7 +8,6 @@ import random
 import time
 import humanfriendly
 from datetime import datetime, timezone, timedelta
-from cap_game import setup
 
 
 # --- Конфигурация бота ---
@@ -28,18 +27,16 @@ import answers
 import actions
 import modcom
 import helpcom
-import cap_game
+import utils
 
 # Передаем бота в модули
 answers.setup(bot)
 actions.setup(bot)
 modcom.setup(bot)
 helpcom.setup(bot)
-cap_game.setup(bot)
+utils.setup(bot)
 
 load_dotenv()
-test_mode = False  # Режим тестирования (без реальных действий)
-
 
 # --- События бота ---
 @bot.event
